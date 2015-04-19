712a2ee4b9ed80fde2505e2e08e56bc3e4605d12
iabdelmenem <iabdelmenem@egypt.ITGSOLUTIONS.COM>
2015-04-19 17:32:50 +0300

    make spinner arabic

--- app/src/main/java/com/rey/material/demo/SpinnersFragment.java
+++ app/src/main/java/com/rey/material/demo/SpinnersFragment.java
@@ -5,13 +5,16 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
-import com.rey.material.widget.FloatingActionButton;
+import com.rey.material.util.LocaleUtil;
 import com.rey.material.widget.Spinner;
+
+import java.util.Locale;
 
 public class SpinnersFragment extends Fragment{
 
@@ -29,7 +32,9 @@ public class SpinnersFragment extends Fragment{
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_spinner, container, false);
-
+        Locale locale = new Locale("en");
+        LocaleUtil.setLocale(getActivity(), locale);
+        Log.e("getLocale", "" + LocaleUtil.getLocale());
         Spinner spn_label = (Spinner)v.findViewById(R.id.spinner_label);
         Spinner spn_no_arrow = (Spinner)v.findViewById(R.id.spinner_no_arrow);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.row_spn, new String[]{"Item 1", "Item 2", "Item 333333333333333"});
